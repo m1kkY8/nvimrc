c62c6ffb090a14581559239b4214bbf8bcb9ec69
Milan Petrovic <5rovicmilan8@gmail.com>
2024-03-21 16:14:27 +0100

    -

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -41,7 +41,7 @@ return {
         vim.keymap.set('n', '<leader>/', function()
             builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
                 winblend = 10,
-                previewer = false, 
+                previewer = false,
             })
         end, { desc = '[/] Fuzzily search in current buffer' })
     end
