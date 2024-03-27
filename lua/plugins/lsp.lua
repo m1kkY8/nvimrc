42ec1b207f3e9b6cdde1ba60a74b070d83aa6d8c
Milan Petrovic <5rovicmilan8@gmail.com>
2024-03-27 16:29:05 +0100

    goto declaration and implementation

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -29,6 +29,8 @@ return {
             local opts = {buffer = bufnr, remap = false}
 
             vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+            vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+            vim.keymap.set("n", "gI", function() vim.lsp.buf.implementation() end, opts)
             vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
             vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
             vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
