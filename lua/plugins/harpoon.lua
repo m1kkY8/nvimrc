1e7e4664869a089760a7674fa844dc3012f51f00
Milan Petrovic <5rovicmilan8@gmail.com>
2024-05-30 13:36:51 +0200

    added new plugins

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,11 +6,30 @@ return {
 
     config = function()
         local harpoon = require("harpoon")
+        harpoon:setup({})
 
-        harpoon:setup()
+        -- basic telescope configuration
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+        local conf = require("telescope.config").values
+        local function toggle_telescope(harpoon_files)
+            local file_paths = {}
+            for _, item in ipairs(harpoon_files.items) do
+                table.insert(file_paths, item.value)
+            end
+
+            require("telescope.pickers").new({}, {
+                prompt_title = "Harpoon",
+                finder = require("telescope.finders").new_table({
+                    results = file_paths,
+                }),
+                previewer = conf.file_previewer({}),
+                sorter = conf.generic_sorter({}),
+            }):find()
+        end
+
+        vim.keymap.set("n", "<C-h>", function() toggle_telescope(harpoon:list()) end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 
         vim.keymap.set("n", "<C-m>", function() harpoon:list():prev() end)
         vim.keymap.set("n", "<C-l>", function() harpoon:list():next() end)
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -27,19 +27,19 @@ return {
 
         lsp_zero.on_attach(function(client, bufnr)
             local opts = {buffer = bufnr, remap = false}
+            local map = vim.keymap.set
 
-            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
-            vim.keymap.set("n", "gI", function() vim.lsp.buf.implementation() end, opts)
-            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-            vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-            vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-            vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+            map("n", "gd", function() vim.lsp.buf.definition() end, opts)
+            map("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+            map("n", "gI", function() vim.lsp.buf.implementation() end, opts)
+            map("n", "K", function() vim.lsp.buf.hover() end, opts)
+            map("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+            map("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+            map("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+            map("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+            map("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+            map("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+            map("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
         end)
 
         require('mason').setup({})
