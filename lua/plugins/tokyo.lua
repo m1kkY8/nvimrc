9254f4085d262d24eb3f58014369112cadd29dad
Milan Petrovic <5rovicmilan8@gmail.com>
2024-06-05 09:40:53 +0200

    backup

--- lua/plugins/tokyo.lua
+++ lua/plugins/tokyo.lua
@@ -18,7 +18,6 @@ return {
         -- Like many other themes, this one has different styles, and you could load
         -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
         vim.cmd.colorscheme 'tokyonight-night'
-
         -- You can configure highlights by doing something like
         vim.cmd.hi 'Comment gui=none'
     end,
