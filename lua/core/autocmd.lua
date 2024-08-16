aa323d3f4b1e49dff30524486e68e83be47f50dd
Milan Petrovic <5rovicmilan8@gmail.com>
2024-08-16 18:27:37 +0200

    nem pojma

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -11,12 +11,22 @@ autocmd('TextYankPost', {
 
 -- Open help pages in vertical splits
 autocmd("FileType", {
-    pattern = "help",
-    command = "wincmd L"
+  pattern = "help",
+  command = "wincmd L"
 })
 
 -- Open man pages in vertical splits
 autocmd("FileType", {
-    pattern = "man",
-    command = "wincmd L"
+  pattern = "man",
+  command = "wincmd L"
 })
+
+vim.api.nvim_create_user_command("DiagnosticToggle", function()
+  local config = vim.diagnostic.config
+  local vt = config().virtual_text
+  config {
+    virtual_text = not vt,
+    underline = not vt,
+    signs = not vt,
+  }
+end, { desc = "toggle diagnostic" })
--- lua/plugins/tokyodark.lua
+++ lua/plugins/tokyodark.lua
@@ -1,11 +1,11 @@
 return {
-    "tiagovla/tokyodark.nvim",
-    opts = {
-        transparent_background = false 
-        -- custom options here
-    },
-    config = function(_, opts)
-        require("tokyodark").setup(opts) -- calling setup is optional
-        vim.cmd [[colorscheme tokyodark]]
-    end,
+  "tiagovla/tokyodark.nvim",
+  opts = {
+    transparent_background = false
+    -- custom options here
+  },
+  config = function(_, opts)
+    require("tokyodark").setup(opts)     -- calling setup is optional
+    vim.cmd [[colorscheme tokyodark]]
+  end,
 }
