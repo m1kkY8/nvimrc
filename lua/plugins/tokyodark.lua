699a7660cfccff2d11d95bccc29faa16e7af037c
Milan Petrovic <5rovicmilan8@gmail.com>
2024-08-17 14:13:41 +0200

    update

--- lua/plugins/tokyodark.lua
+++ lua/plugins/tokyodark.lua
@@ -1,11 +1,11 @@
 return {
   "tiagovla/tokyodark.nvim",
   opts = {
-    transparent_background = false
+    transparent_background = true
     -- custom options here
   },
   config = function(_, opts)
-    require("tokyodark").setup(opts)     -- calling setup is optional
+    require("tokyodark").setup(opts) -- calling setup is optional
     vim.cmd [[colorscheme tokyodark]]
   end,
 }
