73be98121a5c46152744602a1a703e15e8670cfc
Milan Petrovic <5rovicmilan8@gmail.com>
2024-08-26 17:08:35 +0200

    misc

---
+++ lua/plugins/init.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'eandrju/cellular-automaton.nvim',
+  },
+  {
+    'fei6409/log-highlight.nvim',
+    config = function()
+      require('log-highlight').setup {}
+    end,
+  },
+}
