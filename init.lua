fad15ed93699a9154855efc2ad50625dc4993ba4
Milan Petrovic <5rovicmilan8@gmail.com>
2024-09-28 11:38:23 +0200

    update

--- init.lua
+++ init.lua
@@ -3,6 +3,11 @@ require("core/keybinds")
 require("core/autocmd")
 require("core/setopts")
 
---vim.cmd("colorscheme tokyonight-night")
---vim.cmd("colorscheme tokyodark")
-vim.cmd("colorscheme cyberdream")
+
+local themes = {
+  "tokyodark",
+  "tokyonight-night",
+  "cyberdream"
+}
+
+vim.cmd("colorscheme " .. themes[1])
