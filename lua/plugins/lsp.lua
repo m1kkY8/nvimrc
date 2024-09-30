64c232fe7b078de8a23dd55077ee9fcf0e45a643
Milan Petrovic <5rovicmilan8@gmail.com>
2024-09-30 13:39:01 +0200

    lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,7 +6,7 @@ return {
     'williamboman/mason-lspconfig.nvim',
     {
       'VonHeikemen/lsp-zero.nvim',
-      branch = 'v3.x',
+      branch = 'v4.x',
     },
     'hrsh7th/nvim-cmp',
     'hrsh7th/cmp-nvim-lsp',
@@ -24,9 +24,8 @@ return {
 
   config = function()
     local lsp_zero = require('lsp-zero')
-
-    lsp_zero.on_attach(function(client, bufnr)
-      local opts = { buffer = bufnr, remap = false }
+    local lsp_attach = function(client, bufnr)
+      local opts = { buffer = bufnr }
       local map = vim.keymap.set
 
       map("n", "gd", function() vim.lsp.buf.definition() end, opts)
@@ -40,47 +39,60 @@ return {
       map("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
       map("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
       map("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-    end)
+    end
+
+
+    lsp_zero.extend_lspconfig({
+      sign_text = true,
+      lsp_attach = lsp_attach,
+      float_border = "rounded",
+      capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+    })
 
     require('mason').setup({})
     require('mason-lspconfig').setup({
-      ensure_installed = {
-        "lua_ls",
-        "clangd",
-        "gopls",
-        "pyright",
-        "emmet_language_server",
-        "html",
-        "cssls",
-      },
       handlers = {
-        lsp_zero.default_setup,
-        lua_ls = function()
-          local lua_opts = lsp_zero.nvim_lua_ls()
-          require('lspconfig').lua_ls.setup(lua_opts)
+        function(server_name)
+          require('lspconfig')[server_name].setup({})
         end,
       }
     })
 
+
+
     local cmp = require('cmp')
     local cmp_select = { behavior = cmp.SelectBehavior.Select }
+    require('luasnip.loaders.from_vscode').lazy_load()
 
     cmp.setup({
       sources = {
         { name = 'path' },
-        --{name = 'cmdline'},
         { name = 'nvim_lsp' },
         { name = 'nvim_lua' },
         { name = 'luasnip', keyword_length = 2 },
         { name = 'buffer',  keyword_length = 3 },
       },
-      formatting = lsp_zero.cmp_format(),
+
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+        snippet = cmp.config.window.bordered(),
+      },
+
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+      },
+
       mapping = cmp.mapping.preset.insert({
-        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
         ['<C-Space>'] = cmp.mapping.complete(),
       }),
+
     })
   end
 
